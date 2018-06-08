72ef9f9eb9d7da04b7ce01b1949f097e6a17f0a6
Joe Flood <joejimflood@gmail.com>
2018-06-08 19:29:35 -0400

    Update __init__.py

--- binprism/__init__.py
+++ binprism/__init__.py
@@ -2,6 +2,15 @@
 binprism
 
 Tools for fitting linear combinations of continuous basis functions to match binned data.
+
+Often, data from continuous variables are placed into discrete bins.
+BinPrism fits continuous profiles to match these bins, allowing for the ability to
+    *Produce clean visualizations
+    *Estimate results from different bin sizes
+    *Simulate data following a distribution matching the original data.
+    
+ Presently, BinPrism only works for periodic data (such as daily or yearly patterns),
+ but it is hoped that in the future more domains will be supported.
 '''
 from .version import *
 from .core import *

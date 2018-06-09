2bae727752d08f1745c6e76bf726f6ab8fcda8e8
JoeJimFlood <floodjj@uw.edu>
2018-06-08 21:37:24 -0400

    Made final changes for public release

--- binprism/__init__.py
+++ binprism/__init__.py
@@ -2,6 +2,20 @@
 binprism
 
 Tools for fitting linear combinations of continuous basis functions to match binned data.
+
+Often, data from continuous variables are placed into discrete bins.
+BinPrism fits continuous profiles to match these bins, allowing for the ability to
+    *Produce clean visualizations
+    *Estimate results from different bin sizes
+    *Simulate data following a distribution matching the original data.
+    
+ Presently, BinPrism only works for periodic data (such as daily or yearly patterns),
+ but it is hoped that in the future more domains will be supported.
+
+Website: https://github.com/JoeJimFlood/binprism
+Installation Instructions: https://github.com/JoeJimFlood/binprism/blob/master/doc/installation.md
+Descriptions of Classes and Methods: https://github.com/JoeJimFlood/binprism/blob/master/doc/datastructure.md
+Examples: https://github.com/JoeJimFlood/binprism/blob/master/doc/examples.md
 '''
 from .version import *
 from .core import *
